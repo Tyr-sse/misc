21fceb4dbe5ee93bfe03b72c2e3db6e5f6bf311a
F9329132 Thiago de Oliveira Almeida <thiago.sse@bb.com.br>
2022-08-03 18:26:58 -0300

    rct

--- exercicios_js/mod_02/01_todo_list/my-app/src/App.tsx
+++ exercicios_js/mod_02/01_todo_list/my-app/src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import themes from './themes.js';
+
+function frmtd(val:string, sz:number): string{
+  function spcs(n:number){
+    let r = '';
+    for(let i=0;i<n;i++){
+      r+='_';
+    }
+    return r;
+  }
+  let R = val;
+  if(val.length<sz) val+= spcs(sz-val.length);  
+  return val;
+}
+
 
 function App() {
+  //alert(themes)
+
+  genTSK(10);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          taskList.map((x) => {
+            return <ol>
+              <td>{frmtd(x.id+'',20)}</td>
+              <td>{frmtd(x.title+'',20)+'|'}</td>
+              <td>{frmtd(x.status+'',10)?'SIM':'NÃO'}</td>
+              <td>{frmtd(x.dueDate.toDateString(),10)}</td>
+
+            </ol>
+          })
+        }
+      </ul>
     </div>
   );
 }
+
+interface Task {
+  id: number;
+  title: string;
+  status: boolean;
+  dueDate: Date;
+}
+
+let taskList: Task[] = [
+
+
+];
+
+
+function genTSK(n: number) {
+  for (let i = 0; i < n; i++) {
+    taskList.push({
+      id: i,
+      title: 'taskId_' + i,
+      status: i & 1 ? true : false,
+      dueDate: new Date(2022, i % 12, i % 28)
+    })
+  }
+
+}
+
+
+
+
+
 
 export default App;
---
+++ exercicios_js/mod_02/01_todo_list/my-app/src/components/taskList/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+function TaskList(){
+    return <>
+        xeat
+    </>
+}
+export default TaskList;
