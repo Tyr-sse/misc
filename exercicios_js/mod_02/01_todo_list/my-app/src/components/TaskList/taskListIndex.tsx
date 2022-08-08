da9efdd57f0eb95d7e26e871511a226f657c6928
F9329132 Thiago de Oliveira Almeida <thiago.sse@bb.com.br>
2022-08-08 19:20:31 -0300

    asd

--- exercicios_js/mod_02/01_todo_list/my-app/src/components/TaskList/taskListIndex.tsx
+++ exercicios_js/mod_02/01_todo_list/my-app/src/components/TaskList/taskListIndex.tsx
@@ -14,84 +14,23 @@ function leftZeroes(val: (number | string), n: number) {
 
 
 export default function TaskList(props: any) {
-
     let list: any[] = props.taskList ?? [];
-   
-
-    return <Accordion defaultActiveKey="0">
-        {
-            list.map((x) => {
-                let dt = x.dueDate.toString().split(' ');
-                return <>{/*JSON.stringify(x)*/}
-                    <Accordion.Item eventKey={x.id} key={'hd'+x.id} 
-                        style={{//QUADRO
-                            //borderColor: currentTheme.col04,
-                            
-                            //maxWidth: "100%",
-                            borderRadius: 10,
-                            borderWidth: 4,
-                            //margin:5,
-                            borderColor: "#ff00ff",
-                            padding: 1,
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly'
-                    
-                        }} 
-                    >
-                        <Accordion.Header><h3>{x.title}</h3></Accordion.Header>
-                        <Accordion.Body>
-                            Due Date: { leftZeroes(x.dueDate.getDay(),2 )+' / '+leftZeroes(x.dueDate.getMonth() ,2)+' / '+leftZeroes(x.dueDate.getFullYear(),2 )}
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    
-
-                </>
-
-            })
+    return <Acc taskList = {list}/>
 
 
-        }
+}
+
+function Acc(props:any){
+    let list: any[] = props.taskList ?? [];
+    return <>{list.map((x)=>{
+        return <span>A {x.id}</span>
 
 
-
-    </Accordion>
-
+    })}</>
 
 
 }
 
 
-function BasicExample() {
-    return (
-        <Accordion defaultActiveKey="0">
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>Accordion Item #1</Accordion.Header>
-                <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>Accordion Item #2</Accordion.Header>
-                <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
-    );
-}
 
 //export default BasicExample;
--- exercicios_js/mod_02/01_todo_list/my-app/src/components/addTaskForm/AddTaskFormIndex.tsx
+++ exercicios_js/mod_02/01_todo_list/my-app/src/components/addTaskForm/AddTaskFormIndex.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './AddTaskFormStyle.css';
 
 
 export default function AddTaskForm(props: any) {
@@ -7,13 +8,13 @@ export default function AddTaskForm(props: any) {
 
 
     return <div className="row">
-        <input id='title' type='text' placeholder="TITLE" className="in"></input>
+        <input id='title' type='text' placeholder="TITLE" className="in" ></input>
         <input id='priority' type='number' placeholder="PRIORITY" className="in"></input>
         <input id='description' type='text' placeholder="DESCRIPTION" className="in"></input>
-        <div> 
+        <span className="in"> 
             DUE DATE:
-            <input id='descriptions' type='date' ></input>
-        </div>
+            <input id='descriptions' type='date' className="in"></input>
+        </span>
         <button>ADD TASK</button>
 
     </div>
